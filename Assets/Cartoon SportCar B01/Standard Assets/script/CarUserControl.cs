18eaa8c35a686a437b9533eb800b4233445bd6f1
DangNgocDai <dangngocdai998@gmail.com>
2021-01-07 03:33:35 +0700

    commit

--- Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
+++ Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
@@ -4,44 +4,90 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof (CarController))]
-    public class CarUserControl : MonoBehaviour
+    [RequireComponent(typeof(CarController))]
+    public class CarUserControl : SingletonMonoBehaviour<CarUserControl>
     {
         private CarController m_Car; // the car controller we want to use
 
-    float input;
-        private void Awake()
+        public TypeStartusCar typeStartus;
+
+        float input;
+        public override void Awake()
         {
             // get the car controller
+            base.Awake();
             m_Car = GetComponent<CarController>();
         }
 
-// Button Chân Ga: Di lên or xuống. tùy theo biến type của cần gạt
-// Button Chân Phanh: handBrake
-float handbrake = 0;
+        // Button Chân Ga: Di lên or xuống. tùy theo biến type của cần gạt
+        // Button Chân Phanh: handBrake
+        float handbrake = 0;
+        float h;
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update()
+        {
+
+            h = SimpleInput.GetAxis("Horizontal");
+
+            if (typeStartus == TypeStartusCar.N || enterPhanh)
+            {
+                handbrake = 1;
+                return;
+            }
+            else
+            {
+                handbrake = -1;
+            }
+            input = CrossPlatformInputManager.GetAxis("Vertical");
+
+
+            if (CrossPlatformInputManager.GetAxis("Vertical") != 0)
+            {
+                if (typeStartus == TypeStartusCar.D)
+                {
+                    input = 1;
+                }
+                if (typeStartus == TypeStartusCar.R)
+                    input = -1;
+            }
+            else
+            {
+                input = 0;
+            }
+        }
         private void FixedUpdate()
         {
-            // pass the input to the car!
-            float h = SimpleInput.GetAxis("Horizontal");
-            input = CrossPlatformInputManager.GetAxis("Vertical");
-// #if !MOBILE_INPUT
-            handbrake = CrossPlatformInputManager.GetAxis("Jump");
-            Debug.Log(CrossPlatformInputManager.GetAxis("Jump")) ;
-// #endif
- m_Car.Move(h, input, input, handbrake);
+            m_Car.Move(h, input, input, handbrake);
         }
 
-        public void Forward(){
+        bool enterPhanh = false;
+        public void EnterPhanh()
+        {
+            enterPhanh = true;
+        }
+        public void ExitPhanh()
+        {
+            enterPhanh = false;
+        }
+
+        public void Forward()
+        {
             input = 1;
         }
-        public void Backward(){
+        public void Backward()
+        {
             input = -1;
         }
     }
 
-    public enum TypeStartusCar{
-        P,
-        N,
-        F,B
-    }
+
+}
+public enum TypeStartusCar
+{
+    D,
+    N,
+    R
 }
---
+++ Assets/Demo1/Scripts/CangatManager.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.Vehicles.Car;
+
+public class CangatManager : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    Animator animatorCanGat;
+
+    string nameAnim_N = "CanGat_N";
+    string nameAnim_D = "CanGat_D";
+    string nameAnim_R = "CanGat_R";
+
+    int indexCanGat = 1;
+
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    {
+        CarUserControl.Instance.typeStartus = TypeStartusCar.N;
+        animatorCanGat.Play(nameAnim_N);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Vector3 dragVectorDirection = (eventData.position - eventData.pressPosition).normalized;
+        MoveCanGat(GetDragDirection(dragVectorDirection));
+    }
+
+    void MoveCanGat(DraggedDirection direction)
+    {
+        if (direction == DraggedDirection.Up)
+        {
+            Next();
+        }
+        if (direction == DraggedDirection.Down)
+        {
+            Pre();
+        }
+    }
+
+    void Next()
+    {
+        if ((indexCanGat + 1) > 3)
+            return;
+        indexCanGat++;
+        SetAnim();
+    }
+
+    void Pre()
+    {
+        if ((indexCanGat - 1) < 0)
+            return;
+        indexCanGat--;
+        SetAnim();
+    }
+
+    void SetAnim()
+    {
+        switch (indexCanGat)
+        {
+            case 0:
+                animatorCanGat.Play(nameAnim_R);
+                CarUserControl.Instance.typeStartus = TypeStartusCar.R;
+                break;
+            case 1:
+                animatorCanGat.Play(nameAnim_N);
+                CarUserControl.Instance.typeStartus = TypeStartusCar.N;
+                break;
+            case 2:
+                animatorCanGat.Play(nameAnim_D);
+                CarUserControl.Instance.typeStartus = TypeStartusCar.D;
+                break;
+
+        }
+    }
+
+    private enum DraggedDirection
+    {
+        Up,
+        Down,
+        Right,
+        Left
+    }
+    private DraggedDirection GetDragDirection(Vector3 dragVector)
+    {
+        float positiveX = Mathf.Abs(dragVector.x);
+        float positiveY = Mathf.Abs(dragVector.y);
+        DraggedDirection draggedDir;
+        if (positiveX > positiveY)
+        {
+            draggedDir = (dragVector.x > 0) ? DraggedDirection.Right : DraggedDirection.Left;
+        }
+        else
+        {
+            draggedDir = (dragVector.y > 0) ? DraggedDirection.Up : DraggedDirection.Down;
+        }
+        Debug.Log(draggedDir);
+        return draggedDir;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Debug.Log((eventData.position - eventData.pressPosition).y);
+
+        // if (!onDrag && !isMove)
+        // {
+        //     posDragStartY = _container.transform.position.y;
+        //     onDrag = true;
+        // }
+
+
+        // SetPostion((eventData.position - eventData.pressPosition).y * 0.005f);
+    }
+}
